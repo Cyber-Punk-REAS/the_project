e8d945b889a2d0556e1c8813b773647105efe1ff
AlejandroCanoCaldero <alejandrocano.caldero@gmail.com>
2021-04-14 16:20:28 +0200

    first commit

---
+++ Source_code/src/main/java/CyberPunkReas/CovidSimulator/CovidSimulatorApplication.java
@@ -0,0 +1,13 @@
+package CyberPunkReas.CovidSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidSimulatorApplication.class, args);
+	}
+
+}
