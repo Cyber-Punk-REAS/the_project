73cf93556899675b401a9c51266e897b7c7b836b
pablogodiaz <pablogodiaz+1@gmail.com>
2021-04-15 16:33:48 +0200

    Added Region.java specifics

--- Source_code/src/main/java/CyberPunkReas/CovidSimulator/models/Region.java
+++ Source_code/src/main/java/CyberPunkReas/CovidSimulator/models/Region.java
@@ -1,8 +1,67 @@
 package CyberPunkReas.CovidSimulator.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.ArrayList;
 
+@Entity
 public class Region {
-    private ArrayList<Person> listPeople;
+
+    @Id
+    @GeneratedValue
+    private ArrayList<Person> people;
+    private ArrayList<Business> businesses;
+
+    public Region (ArrayList<Person> listPeople, ArrayList<Business> listBusinesses) {
+        this.people = listPeople;
+        this.businesses = listBusinesses;
+    }
+
+    public Region () {
+        this.people = new ArrayList<>();
+        this.businesses = new ArrayList<>();
+    }
+
+    public ArrayList<Person> getPeople() { return people; }
+
+    public ArrayList<Business> getBusinesses() { return businesses; }
+
+    public void addPerson(Person p) { this.people.add(p); }
+
+    public void addBusiness(Business b) { this.businesses.add(b); }
+
+    public void setPeople(ArrayList<Person> plist) { this.people = plist; }
+
+    public void setBusinesses(ArrayList<Business> blist) { this.businesses = blist; }
+
+    @Override
+    public int hashCode() {
+        int res = 0;
+        for (Person p : people) {
+            res += p.hashCode();
+        }
+        for(Business b : businesses) {
+            res += b.hashCode();
+        }
+        return res;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Region other = (Region) obj;
+        boolean res = true;
+        for (Person p : people) {
+            res = other.people.contains(p);
+        }
+        for(Business b : businesses) {
+            res = other.businesses.contains(b);
+        }
+        return res;
+    }
 
 }
