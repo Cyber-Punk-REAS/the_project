87cc67a5de289e5481418aeeb68c2f82a1bcf505
Pablo <pablogodiaz@gmail.com>
2021-05-17 10:22:00 +0200

    Added restriction.java code

--- Source_code/src/main/java/CyberPunkReas/CovidSimulator/models/Restriction.java
+++ Source_code/src/main/java/CyberPunkReas/CovidSimulator/models/Restriction.java
@@ -1,5 +1,50 @@
 package CyberPunkReas.CovidSimulator.models;
 
 public class Restriction {
-    
+
+    private boolean curfew;
+    private boolean businessEssential;
+    private boolean businessNonEssential;
+    private int timeRestriction;
+
+    public Restriction() {
+        curfew = false;
+        businessEssential = false;
+        businessNonEssential = false;
+        timeRestriction = 0;
+    }
+
+    public boolean isCurfew() {
+        return curfew;
+    }
+
+    public boolean isBusinessEssential() {
+        return businessEssential;
+    }
+
+    public boolean isBusinessNonEssential() {
+        return businessNonEssential;
+    }
+
+    public int getTimeRestriction() {
+        return timeRestriction;
+    }
+
+    public void setCurfew(boolean curfew) {
+        this.curfew = curfew;
+    }
+
+    public void setBusinessEssential(boolean businessEssential) {
+        this.businessEssential = businessEssential;
+        if(businessEssential = false) { this.businessNonEssential = false; }
+    }
+
+    public void setBusinessNonEssential(boolean businessNonEssential) {
+        this.businessNonEssential = businessNonEssential;
+    }
+
+    public void setTimeRestriction(int timeSpan) {
+        this.timeRestriction = timeSpan;
+    }
+
 }
