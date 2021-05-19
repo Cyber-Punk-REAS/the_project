f3c809e79cc05a63bf3ca9808d6eef07f1f94a6c
n4xo <ilopezosaserrano@gmail.com>
2021-05-19 14:29:26 +0200

    Fixed empty parameters and redundant relations from Region class and created Test Plans for Region

---
+++ Code/Master/Design_Model/Region.java
@@ -0,0 +1,45 @@
+/**
+ * @(#) Region.java
+ */
+
+package Master.Design_Model;
+
+public class Region
+{
+	private Simulation simulation2;
+	
+	private Area areas2;
+	
+	private Simulation simulation;
+	
+	private Area areas;
+	
+	private SUD sud;
+	
+	public void setRegionName( String name )
+	{
+		
+	}
+	
+	public void setAreasAmount( int areasAmount )
+	{
+		
+	}
+	
+	public void addEmptyArea( )
+	{
+		
+	}
+	
+	public void deleteArea( Area area )
+	{
+		
+	}
+	
+	public void deleteRegion( )
+	{
+		
+	}
+	
+	
+}
