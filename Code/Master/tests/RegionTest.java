cc1694521825ab12d815da70c10452414ce19485
N4xo <n4xo@pm.me>
2021-05-19 17:21:55 +0200

    Added Test Plans

---
+++ Code/Master/tests/RegionTest.java
@@ -0,0 +1,49 @@
+package Master.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+class RegionTest {
+	
+	/** This should create a valid region to make the tests with
+	 * */
+	@BeforeClass
+	void setRegion() {
+		
+	}
+	
+	/** This checks whether the name is set correctly for the given region
+	 * */
+	@Test
+	void testSetRegionName() {
+		fail("Not yet implemented");
+	}
+
+	/** This checks whether the amount of areas is set correctly for the 
+	 * given region
+	 * */
+	@Test
+	void testSetAreasAmount() {
+		fail("Not yet implemented");
+	}
+
+	/** This checks whether an empty area is added correctly to the Areas[] 
+	 * for the given region
+	 * */
+	@Test
+	void testAddEmptyArea() {
+		fail("Not yet implemented");
+	}
+	
+
+	/** This checks whether the areas are deleted correctly from the Areas[] 
+	 * for the given region
+	 * */
+	@Test
+	void testDeleteArea() {
+		fail("Not yet implemented");
+	}
+
+}
