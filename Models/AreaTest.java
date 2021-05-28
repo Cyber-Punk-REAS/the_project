c5caa01ce520e4c007c9039fe3fdf14bc7cbc675
Capobian <81689729+Capobian@users.noreply.github.com>
2021-05-28 12:48:34 +0200

    TestPlanArea

---
+++ Models/AreaTest.java
@@ -0,0 +1,28 @@
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AreaTest {
+	
+	
+	/* This should create a valid Area in order to test it */
+	
+	@BeforeClass
+	void setArea() {}
+		
+	/* This should check if the population of the area is correctly set*/
+	@Test
+	void TestPopulation() {}
+	
+	/* This should check if business parameters are properly inserted */
+	@Test
+	void TestBusiness() {}
+	
+	/*This should check if the restriction policy is properly added*/
+	@Test
+	void TestPolicy () {} 
+	
+	/*This should check if a policy is correctly unlinked to the area*/
+	@Test 
+	void TestUnlink () {}	
+	
+}
