1e9c68fabcf5b9f02b4f2c7805f96cc302057875
JesusVillenRuiz <jesusvillenruiz2001@uma.es>
2021-06-06 16:50:55 +0200

    Add SourceCode folder

---
+++ Source_code/src/test/java/CyberPunkReas/CovidSimulator/CovidSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package CyberPunkReas.CovidSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
