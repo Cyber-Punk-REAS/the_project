fd8eb4ed4c6ce2852e91f4b512ba5ff8a0b05f07
N4xo <n4xo@pm.me>
2021-06-07 19:37:01 +0200

    Modified Person.java

--- Source/src/main/java/CyberPunkReas/CovidSimulator/models/Person.java
+++ Source/src/main/java/CyberPunkReas/CovidSimulator/models/Person.java
@@ -1,5 +1,8 @@
 package CyberPunkReas.CovidSimulator.models;
 
+import java.util.List;
+import java.util.Random;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,125 +12,54 @@ import javax.persistence.ManyToOne;
 public class Person {
 	@Id
 	@GeneratedValue
-	private Integer dni;
-	private int age;
-	private boolean isImmune;
-	private float ChInfect;
-	private float ChInfected;
-	private float ChToGoOutAtNight;
-	private int businessVisited;
 	
 	@ManyToOne
-	private Region region;
-	
-	public Person(Integer dni, int age, boolean isImmune, float chInfect, float chInfected, float chToGoOutAtNight, int businessVisited) {
-		this.dni = dni;
-		this.age = age;
-		this.isImmune = isImmune;
-		ChInfect = chInfect;
-		ChInfected = chInfected;
-		ChToGoOutAtNight = chToGoOutAtNight;
-		this.businessVisited = businessVisited;
-	}
-	
-	
-	
-	public Region getRegion() {
-		return region;
-	}
+    private static int _id_ = 0;
+    private Random random = new Random();
+    private int id;
+    private Area area;
+    private boolean startingImmunity;
+    private boolean alive;
+    private boolean employed;
+    private Business business;
+    private double contagiousness;
+    private Person[] friends;
 
-	public Integer getDni() {
-		return dni;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	
-	public void setDni(Integer dni) {
-		this.dni = dni;
-	}
-	
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	public boolean isImmune() {
-		return isImmune;
-	}
-	
-	public float getChInfect() {
-		return ChInfect;
-	}
-	
-	public float getChInfected() {
-		return ChInfected;
-	}
-	
-	public float getChToGoOutAtNight() {
-		return ChToGoOutAtNight;
-	}
-	
-	public int getBusinessVisited() {
-		return businessVisited;
-	}
-	
-	public void setImmune(boolean isImmune) {
-		this.isImmune = isImmune;
-	}
-	
-	public void setChInfect(float chInfect) {
-		ChInfect = chInfect;
-	}
-	
-	public void setChInfected(float chInfected) {
-		ChInfected = chInfected;
-	}
-	
-	public void setChToGoOutAtNight(float chToGoOutAtNight) {
-		ChToGoOutAtNight = chToGoOutAtNight;
-	}
-	
-	public void setBusinessVisited(int businessVisited) {
-		this.businessVisited = businessVisited;
-	}
-	
-	public void setRegion(Region region) {
-		this.region = region;
-	}
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (dni == null) {
-			if (other.dni != null)
-				return false;
-		} else if (!dni.equals(other.dni))
-			return false;
-		return true;
-	}
-	
-	@Override
-	public String toString() {
-		return "Person [DNI = " + dni + ", Age = " + age + ", isImmune = " + isImmune + ", Chance to infect = " + ChInfect
-				+ ", Chance to be infected=" + ChInfected + ", Chance to go out at night = " + ChToGoOutAtNight + ", business visited per day = "
-				+ businessVisited + "]";
-	}
-	
-	
+    public Person(Area area){
+        this.id = Person._id_++;
+        this.area = area;
+        this.startingImmunity = false;
+        this.employed = false;
+        this.alive = true;
+    }
+
+    public void setBusiness(Business business){
+        if(employed)
+            this.business = business;
+        else
+            this.business = null;
+    }
+
+    public void setStartingImmunity(int immunityChance){
+        if(random.nextInt(100) < immunityChance)
+            startingImmunity = true;
+    }
+
+    public void setWork(int unemployment){
+        if(random.nextInt(100) > unemployment)
+            employed = true;
+    }
+
+    public void setFriends(double averageFriends, double friendsStandardDeviation, List<Person> people){
+        for(int i = 0; i < Person.rndFriends( averageFriends, friendsStandardDeviation); i++)
+            friends[i] = people.get(random.nextInt(people.size()));
+    }
+
+    
+    private static int rndFriends(double averageFriends, double friendsStandardDeviation){
+        Random rnd = new Random();
+        
+        return (int) ((rnd.nextGaussian() + averageFriends) * friendsStandardDeviation);    }
+    
+    
 }
