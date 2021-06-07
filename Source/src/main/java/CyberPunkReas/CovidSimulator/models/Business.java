b03b90daad3b597cb431942d51a6dbd67862364f
N4xo <n4xo@pm.me>
2021-06-07 20:57:41 +0200

    Business created

--- Source/src/main/java/CyberPunkReas/CovidSimulator/models/Business.java
+++ Source/src/main/java/CyberPunkReas/CovidSimulator/models/Business.java
@@ -1,38 +1,39 @@
 package CyberPunkReas.CovidSimulator.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Business {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	private boolean isEsential;
-	private int maxPeople;
-	private boolean isIndoors;
+	private boolean essential;
+	
+	@OneToMany
+	private List<Person> workers;
 	
 	@ManyToOne
-	private Region r;
+	private Area area;
 	
-	public Business(Integer id, boolean esential, int maxPeople, boolean indoors)
+	public Business(Integer id, boolean esential, Area area)
 	{
 		this.id = id;
-		this.isEsential = esential;
-		this.maxPeople = maxPeople;
-		this.isIndoors = indoors;
-	}
-	
-	
+		this.essential = essential;
+		this.area = area;
+	}	
 	
 	public boolean isEsential() {
-		return isEsential;
+		return essential;
 	}
 
-	public Region getR() {
-		return r;
+	public Region getArea() {
+		return area;
 	}
 
 	public Integer getId()
@@ -42,17 +43,7 @@ public class Business {
 	
 	public boolean getType() 
 	{
-		return isEsential;
-	}
-	
-	public int getMaxPeople() 
-	{
-		return maxPeople;
-	}
-	
-	public boolean isIndoors() 
-	{
-		return isIndoors;
+		return essential;
 	}
 	
 	public void setId(Integer id)
@@ -62,53 +53,23 @@ public class Business {
 
 	public void setType(boolean isEsential) 
 	{
-		this.isEsential = isEsential;
+		this.essential = isEsential;
 	}
 	
-	public void setMaxPeople(int maxPeople) 
-	{
-		this.maxPeople = maxPeople;
+	public List<Person> getWorkers() {
+		return workers;
+	}
+
+	public void setWorkers(List<Person> workers) {
+		this.workers = workers;
 	}
 	
-	public void setIndoors(boolean isIndoors) 
-	{
-		this.isIndoors = isIndoors;
-	}
-	
-	public void setEsential(boolean isEsential) {
-		this.isEsential = isEsential;
+	public void addWorker(Person person) {
+		workers.add(person);
 	}
 
-	public void setR(Region r) {
-		this.r = r;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (isEsential ? 1231 : 1237);
-		result = prime * result + (isIndoors ? 1231 : 1237);
-		result = prime * result + maxPeople;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Business other = (Business) obj;
-		if (isEsential != other.isEsential)
-			return false;
-		if (isIndoors != other.isIndoors)
-			return false;
-		if (maxPeople != other.maxPeople)
-			return false;
-		return true;
+	public void setArea(Area area) {
+		this.area = area;
 	}
 	
 }
