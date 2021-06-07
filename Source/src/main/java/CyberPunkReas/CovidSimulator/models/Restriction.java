ad98ecdaaa37758a6509c2a2bdb0f35b25a6b997
selimtraore <selimtraore@live.com>
2021-06-07 18:33:04 +0200

    Class Population Class Area Own Version

--- Source/src/main/java/CyberPunkReas/CovidSimulator/models/Restriction.java
+++ Source/src/main/java/CyberPunkReas/CovidSimulator/models/Restriction.java
@@ -1,5 +1,53 @@
 package CyberPunkReas.CovidSimulator.models;
 
 public class Restriction {
+	
+	
+	
+	private boolean curfew;
+	private boolean EssentialBussiness;
+	private int timeRestriction;
+	
+	
+	
+	public Restriction() {
+		
+	}
+
+
+
+	public boolean isCurfew() {
+		return curfew;
+	}
+
+
+
+	public void setCurfew(boolean curfew) {
+		this.curfew = curfew;
+	}
+
+
+
+	public boolean isEssentialBussiness() {
+		return EssentialBussiness;
+	}
+
+
+
+	public void setEssentialBussiness(boolean essentialBussiness) {
+		EssentialBussiness = essentialBussiness;
+	}
+
+
+
+	public int getTimeRestriction() {
+		return timeRestriction;
+	}
+
+
+
+	public void setTimeRestriction(int timeRestriction) {
+		this.timeRestriction = timeRestriction;
+	}
     
 }
